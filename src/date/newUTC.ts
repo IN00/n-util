a89273a9be565a32e6449cbbf50ec112230377e3
Naoya Iwashita <>
2023-10-10 14:49:57 +0900

    Modify tsconfig

--- src/date/newUTC.ts
+++ src/date/newUTC.ts
@@ -1,4 +1,4 @@
-import { newDate } from "./newDate"
+import { newDate } from "./newDate.ts"
 
 export const newUTC = (year: number, month: number, day: number, hour: number, minute: number, second: number) => {
 	newDate(year, month, day, hour - 9, minute, second)
